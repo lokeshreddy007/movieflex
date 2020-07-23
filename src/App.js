eac258916de0f95ac77d6116f6f01a04fc474fd0
lokeshreddy007 <lokeshreddy160@gmail.com>
2020-07-23 16:44:42 +0530

    Add CSS for Posters Horizontal with Scroll effect

--- src/App.js
+++ src/App.js
@@ -9,6 +9,13 @@ function App() {
     <div className="App">
       <Row title="Nexflex Originals" fetchUrl={requests.fetchNetflexOriginals}/>
       <Row title="Trending Now" fetchUrl={requests.fetchTrending}/>
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated}/>
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies}/>
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies}/>
+      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies}/>
+      <Row title="Ramance Movies" fetchUrl={requests.fetchRomanceMovies}/>
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries}/>
+
     </div>
   );
 }
--- src/Row.js
+++ src/Row.js
@@ -1,7 +1,7 @@
     import React, { useState, useEffect } from 'react'
 
     import axios from './axios'
-
+    import "./Row.css"
     const imageBaseUrl = "https://image.tmdb.org/t/p/original/";
 
     function Row({ title, fetchUrl }) {
@@ -20,7 +20,13 @@
             <div className="row">
                <h2>{title}</h2>
                <div className="row__posters">
-               		{movies.map(movie => (<img className="row__poster" src=`${imageBaseUrl}${movie.poster_path}` alt={movie.name} />))}
+               		{movies.map(movie => (
+               			<img 
+               			key={movie.id}
+               			className="row__poster" 
+               			src={`${imageBaseUrl}${movie.poster_path}`} 
+               			alt={movie.name} />
+               		))}
                </div>
             </div>
         )
